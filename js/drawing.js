a0d44ac031db08cde2657c5a4d71def6eb076ae5
Alastair McGowan-Douglas <alastair.douglas+github@gmail.com>
2010-09-17 21:19:00 +0100

    Double click to end line

--- js/drawing.js
+++ js/drawing.js
@@ -3,6 +3,7 @@ var Canvas = (function(element,_h,_w){
         mousePos = {},
         handlers = {
             click: function(e){},
+            dblclick: function(e){},
             mousemove: function(e){}
         },
         paper,
@@ -16,6 +17,9 @@ var Canvas = (function(element,_h,_w){
 
     clickZone.click(function(e) {
         handlers.click(e);
+    });
+    clickZone.dblclick(function(e) {
+        handlers.dblclick(e);
     });
     clickZone.mousemove(function(e){
         handlers.mousemove(e);
@@ -32,15 +36,29 @@ var Canvas = (function(element,_h,_w){
         var l = paper.path(p.svgString());
         l.attr({ stroke: "rgb(0,0,0)", "stroke-width": 1 });
 
+        // If we click the line by accident it should act as though we'd clicked
+        // the canvas, which is easy if we don't worry about mouse location.
+        p.click =
+        handlers.click = function(e) {
+            p.addNode();
+            l.attr({ path: p.svgString() });
+        };
+ 
+        // set it up so that clicking this line calls the handler on the Path
+        l.click(function(e) { p.click(e); });
+
+        p.dblclick = 
+        handlers.dblclick = function(e) {
+            self.mode('line');
+            handlers.mousemove = function(e){};
+        };
+        l.dblclick(function(e){ p.dblclick(e) });
+
         handlers.mousemove = function(e) {
             p.moveNode(-1, e.offsetX, e.offsetY);
             l.attr({ path: p.svgString() });
         };
         
-        handlers.click = function(e) {
-            p.addNode(e.offsetX, e.offsetY);
-            l.attr({ path: p.svgString() });
-        };
     };
 });
 
