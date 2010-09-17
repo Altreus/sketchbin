699b7f76ee60d257ab35dc26b9f463c3f5196305
Alastair McGowan-Douglas <alastair.douglas+github@gmail.com>
2010-09-18 00:50:07 +0100

    Show stroke tools when Line is selected.

--- js/tools.js
+++ js/tools.js
@@ -1,5 +1,14 @@
 $(function() {
     $('#line').click(function(e) {
+        showLineToolSettings();
         drawCanvas.mode('line');
     });
 });
+
+function showLineToolSettings() {
+    var div = $('#tool-settings');
+
+    div.find('fieldset').appendTo($('#spare-tools-settings'));
+
+    $('#inputs-stroke').appendTo(div);
+}
