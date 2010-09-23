1a574957becedc6c88f0c33513b4ef270b74e8c8
Alastair McGowan-Douglas <alastair.douglas+github@gmail.com>
2010-09-23 22:33:29 +0100

    KISS. Store -ve height & width and work out pos
    trying to work out the new position whenever the mouse changed was hard
    so why not just store a negative height and width and use them to decide
    where to actually draw the real rect?

--- js/shape.js
+++ js/shape.js
@@ -1,25 +1,12 @@
 var Rect = function(_x, _y, _h, _w, _r) {
     var self = this;
-    self.x = _x + 0.5; self.y = _y + 0.5;
-    self.h = _h + 0.5; self.w = _w + 0.5;
+    self.x = _x; self.y = _y;
+    self.h = _h; self.w = _w;
     self.r = _r;
 
     self.resize = function(_h,_w) {
-        if( _h < 0) {
-            self.y = self.y + self.h + (_h - 0.5);
-            self.h = Math.abs(_h);
-        }
-        else {
-            self.h = _h + 0.5;
-        }
-        
-        if( _w < 0) {
-            self.x = self.x + self.w + (_w - 0.5);
-            self.w = Math.abs(_w) + 0.5;
-        }
-        else {
-            self.w = _w + 0.5;
-        }
+        self.h = _h;
+        self.w = _w;
     };
 
     self.corners = function(_r) {
@@ -30,7 +17,7 @@ var Rect = function(_x, _y, _h, _w, _r) {
 var Ellipse = function(_x, _y, _rx, _ry) {
     var self = this;
 
-    self.x = _x + 0.5; self.y = _y + 0.5;
+    self.x = _x; self.y = _y;
     self.rx = _rx; self.ry = _ry;
 
     self.resize = function(x, y) {
